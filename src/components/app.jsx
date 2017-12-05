6dc2f3f4c90a67a6359226984272add71cd2d91d
Jean-Philippe Roy <jp.roy.84@gmail.com>
2017-12-05 11:34:34 +0100

    app layout

---
+++ src/components/app.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import FlatList from './flat_list.jsx'
+import Flat from './flat.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <div className="flat-list">
+
+          </div>
+          <div class="map-container">
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App
